31e23f82cb65bf14e44aff2e5b4893b78600e111
Eric Stratton <strattonericj@gmail.com>
2021-01-15 08:35:21 -0800

    remove unecessary files and their references

--- Treats/Controllers/HomeController.cs
+++ Treats/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PierresTreats.Solution.Models;
+using Treats.Models;
 
-namespace PierresTreats.Solution.Controllers
+namespace Treats.Controllers
 {
     public class HomeController : Controller
     {
@@ -15,15 +15,6 @@ namespace PierresTreats.Solution.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
