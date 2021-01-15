60858492553acd538ac15be071e3341203be69fd
Eric Stratton <strattonericj@gmail.com>
2021-01-15 15:14:43 -0800

    add order reference to context file

--- Treats/Models/TreatsContext.cs
+++ Treats/Models/TreatsContext.cs
@@ -8,6 +8,7 @@ namespace Treats.Models
     public DbSet<Flavor> Flavors { get; set; }
     public DbSet<Treat> Treats { get; set; }
     public DbSet<TreatFlavor> TreatFlavor { get; set; }
+    public DbSet<Order> Orders { get; set; }
 
     public TreatsContext(DbContextOptions options) : base(options) { }
   }
