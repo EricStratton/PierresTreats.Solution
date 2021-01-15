b5ab5e34ed7e135239ba185a9b1ce9c7d49835a8
Eric Stratton <strattonericj@gmail.com>
2021-01-15 14:55:36 -0800

    add dictionary with db data for home view

--- Treats/Controllers/HomeController.cs
+++ Treats/Controllers/HomeController.cs
@@ -10,10 +10,22 @@ namespace Treats.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly TreatsContext _db;
+
+        public HomeController(TreatsContext db)
+        {
+            _db = db;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            var flavors = _db.Flavors.ToList();
+            var treats = _db.Treats.ToList();
+            Dictionary<string, object> model = new Dictionary<string, object> { };
+            model.Add("flavor", flavors);
+            model.Add("treat", treats);
+            return View(model);
         }
 
-    }
+    }       
 }
