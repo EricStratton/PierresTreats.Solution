1dbda3dda0a6abca87f52bb8b24808d030a470e0
Eric Stratton <strattonericj@gmail.com>
2021-01-15 09:01:03 -0800

    update context file to hold models

--- Treats/Models/TreatsContext.cs
+++ Treats/Models/TreatsContext.cs
@@ -5,6 +5,9 @@ namespace Treats.Models
 {
   public class TreatsContext : IdentityDbContext<ApplicationUser>
   {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
 
     public TreatsContext(DbContextOptions options) : base(options) { }
   }
